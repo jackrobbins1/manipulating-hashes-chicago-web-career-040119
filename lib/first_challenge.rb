3e61ac0633a7af06f6d68ff9de81c89bb5f9b41d
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 19:54:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          
+
         end
       end
     end
