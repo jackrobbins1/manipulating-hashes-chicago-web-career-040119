2ae3a5fc20571913f2a8ca9998e9a19526bad015
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 20:03:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,13 +16,16 @@ def first_challenge
   }
 
   contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        value.delete_if do |flavor|
-          flavor == strawberry
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_icecream_flavors
+          value.delete_if do |flavor|
+            flavor == strawberry
+          end
         end
       end
     end
+
   end
   #remember to return your newly altered contacts hash!
   contacts
